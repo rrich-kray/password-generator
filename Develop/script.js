7925380322aa444ab3fa7df59a08f60e0c513a3e
rrich-kray <rrich.kray@gmail.com>
2021-11-28 20:44:11 -0500

    streamlined js

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,22 @@
 // Assignment code here
 
-const all = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()0123456789";
-const alphanumeric = all.slice(all.indexOf("a"), all.indexOf("z")) + all.slice(all.indexOf('0'), all.indexOf('9'));
-const miscNumbers = all.slice(all.indexOf("!"), all.indexOf("9"));
-const alphaMisc = all.slice(all.indexOf('a'), all.indexOf(')'));
+const all = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()0123456789";
+const alphabet = all.slice(all.indexOf("a"), all.indexOf("z"));
+const alphabetCapital = all.slice(all.indexOf("A"), all.indexOf("Z"));
+const special = all.slice(all.indexOf('!'), all.indexOf(')'));
+const numbers = all.slice(all.indexOf('0'), all.indexOf('9'));
+const options = [alphabet, alphabetCapital, special, numbers];
 
 const generatePassword = () => {
   var password = "";
   var counter = 0;
   var length = window.prompt("Please specify a length between 8 and 128 characters for your password");
-  var content = window.prompt("Please specify '1' for 'all', '2' for 'alphanumeric', '3' for miscellaneous characters and numbers, or '4' for alphabetical and miscellaneous characters")
-  if (length >= 8 && length <= 128 && (content==='1' || content==='2' || content==='3' || content==='4')) {
+  var content = window.prompt("Please specify '1' for 'all', '2' for 'alphanumeric', '3' for miscellaneous characters and numbers, or '4' for alphabetical and miscellaneous characters");
+  if (length >= 8 && length <= 128) {
+    var choiceString = "" 
+    content.replace(' ', '').split(',').forEach((choice) => {choiceString += options[choice]});
     while (counter <= length){
-      if (content==='1') {
-        password += all[Math.floor(Math.random() * all.length)];
-      } else if (content==='2') {
-        password += alphanumeric[Math.floor(Math.random() * alphanumeric.length)];
-      } else if (content==='3'){
-        password += miscNumbers[Math.floor(Math.random() * miscNumbers.length)];
-      } else if (content==='4') {
-        password += alphaMisc[Math.floor(Math.random() * alphaMisc.length)];
-      }
+      password += choiceString[Math.floor(Math.random() * choiceString.length)];
       counter++;
     }
     return password;
