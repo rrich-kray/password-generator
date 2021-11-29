d6ee484b2aceeb2c7fd093cf69e515f59b00cab2
rrich-kray <rrich.kray@gmail.com>
2021-11-28 19:47:35 -0500

    Improved JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,26 @@
 // Assignment code here
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()0123456789";
+const all = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()0123456789";
+const alphanumeric = all.slice(all.indexOf("a"), all.indexOf("z")) + all.slice(all.indexOf('0'), all.indexOf('9'));
 
 const generatePassword = () => {
   var password = "";
   var counter = 0;
-  var length = window.prompt("Please specify your desired password length");
-  while (counter <= length){
-    password += alphabet[Math.floor(Math.random() * alphabet.length)];
-    counter++;
+  var length = window.prompt("Please specify a length between 8 and 128 characters for your password");
+  var content = window.prompt("Please specify '1' for 'all' or '2' for 'alphanumeric'")
+  if (length >= 8 && length <= 128 && (content==='1' || content==='2')) {
+    while (counter <= length){
+      if (content==='1') {
+        password += all[Math.floor(Math.random() * all.length)];
+      } else if (content==='2') {
+        password += alphanumeric[Math.floor(Math.random() * alphanumeric.length)];
+      }
+      counter++;
+    }
+    return password;
+  } else {
+    window.alert('Please provide valid input');
   }
-  return password;
 }
 
 // Get references to the #generate element
@@ -22,6 +32,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
