9307d9680443d9693d2d6885f39902bd56fa1e15
rrich-kray <rrich.kray@gmail.com>
2021-11-28 19:12:09 -0500

    Updated JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,14 @@
 // Assignment code here
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
-const characters = "!@#$%^&*()0123456789";
-const all = alphabet + characters;
+const alphabet = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()0123456789";
 
 const generatePassword = () => {
   var password = "";
   var counter = 0;
   var length = window.prompt("Please specify your desired password length");
-  var included = confirm("Select 'yes' to include all available character; select 'no' to include only numbers")
   while (counter <= length){
-    if (included){
-      password += all[Math.floor(Math.random() * all.length)];
-      counter++;
-    } else {
-      password += alphabet[Math.floor(Math.random() * alphabet.length)]
-      counter++;
-    }
+    password += alphabet[Math.floor(Math.random() * alphabet.length)];
+    counter++;
   }
   return password;
 }
@@ -30,7 +22,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
